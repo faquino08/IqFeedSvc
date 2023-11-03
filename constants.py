e05bbf9ec9e396971052e3736ddc36b5f7f62a9f
faquino08 <faquino0896@gmail.com>
2023-11-03 18:50:53 -0400

    Constants.py was being ignored by git by accident.

---
+++ constants.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+from os import environ
+
+PROJECT_ROOT = environ['PROJECT_ROOT']
+POSTGRES_LOCATION = environ['POSTGRES_LOCATION']
+POSTGRES_PORT = environ['POSTGRES_PORT']
+POSTGRES_DB = environ['POSTGRES_DB']
+POSTGRES_USER = environ['POSTGRES_USER']
+POSTGRES_PASSWORD = environ['POSTGRES_PASSWORD']
+IQ_LOCATION = environ['IQ_LOCATION']
+IQ_PORT = environ['IQ_PORT']
+DEBUG = environ['DEBUG_BOOL']
+DEFAULT_MIN_START = environ.get('DEFAULT_MIN_START','20200101 073000')
+DEFAULT_DAILY_START = environ.get('DEFAULT_DAILY_START','20180101')
+APP_NAME = environ.get('APP_NAME','IqHistory')
